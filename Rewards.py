b95f349da88bd57f7fd7c4baf73dad7b29989537
roamiri <amiriruhollah@gmail.com>
2018-02-07 18:02:45 -0700

    working on IL vs CL ; different action sizes; different choosing of actions

--- Rewards.py
+++ Rewards.py
@@ -6,3 +6,8 @@ def R_1(p1, p2, g1, g2, beta, sigma2, gamma):
     gamma2 = (g2*p2) / (g2*p1*beta + sigma2)
     reward = np.log2(1+gamma1/gamma) + np.log2(1+gamma2/gamma)
     return reward
+
+def R_2 (s, i, sigma2):
+
+    R = np.log2( 1.0 + s/(i + sigma2))
+    return R
