b5627ef6cc6805dbde8865b029412ddb3430e35a
roamiri <amiriruhollah@gmail.com>
2018-03-13 21:31:49 -0600

    3d animation for two user interf chananel is working

--- pa_slide_bar.py
+++ pa_slide_bar.py
@@ -4,69 +4,113 @@ import matplotlib.pyplot as plt
 from matplotlib.mlab import bivariate_normal
 from matplotlib.widgets import Slider, Button
 
-#Setup figure and data
-fig, ax = plt.subplots()
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import matplotlib, time
+
+class plot3dClass( object ):
+
+    def __init__( self):
+        # Channel conditions
+        self.g1 = 2.5
+        self.g2 = 1.5
+        self.p1_max = 10
+        self.p2_max = 100
+        self.sigma2 = 1
+        self.Gamma = 1
+
+        # Make data.
+        self.p1 = np.linspace(0, self.p1_max, 100)
+        self.p2 = np.linspace(0, self.p2_max, 100)
+        self.X, self.Y = np.meshgrid(self.p1, self.p2)
+
+        self.fig = plt.figure()
+        self.ax = self.fig.gca(projection='3d')
+        self.fig.subplots_adjust(bottom=0.25)
+
+        # self.fig, self.ax = plt.subplots()
+        # plt.subplots_adjust(bottom=0.25)
+
+        self.ax.w_zaxis.set_major_locator( LinearLocator( 10 ) )
+        self.ax.w_zaxis.set_major_formatter( FormatStrFormatter( '%.03f' ) )
+
+        self.Z = self.throughput(0.0)
+
+        self.surf = self.ax.plot_surface(self.p1, self.p2, self.Z, cmap=cm.coolwarm, linewidth=0, antialiased=False )
+
+        self.axcolor = 'lightgoldenrodyellow'
+        self.ax_slider = plt.axes([0.25, 0.1, 0.65, 0.03], axisbg=self.axcolor)
+        self.slider_beta = Slider(self.ax_slider, 'Beta', 0.0, 1.0, valinit=0.0)
+        self.slider_beta.on_changed(self.drawNow)
+        self.fig.colorbar(self.surf, shrink=0.5, aspect=5)
+        self.fig.show() # maybe you want to see this frame?
+
+    def throughput(self,beta):
+        R1 = np.log2(1 + (self.g1 * self.p1) / ((self.g1 * self.p2 * beta + self.sigma2) * self.Gamma))
+        R2 = np.log2(1 + (self.g2 * self.p2) / ((self.g2 * self.p1 * beta + self.sigma2) * self.Gamma))
+        R3d = R1 + R2
+        return R3d
+
+    def drawNow( self, val):
+        self.Z = self.throughput(val)
+        self.surf.remove()
+        self.surf = self.ax.plot_surface(self.p1, self.p2, self.Z, cmap=cm.coolwarm, linewidth=0, antialiased=False )
+        plt.draw()                      # redraw the canvas
+        # time.sleep(1)
+
+# matplotlib.interactive(True)
+# p = plot3dClass()
+# while(1):
+#     p.fig.show()
+
+g1 = 2.5
+g2 = 1.5
+p1_max = 10
+p2_max = 20
+sigma2 = 1
+Gamma = 1
+
+# Make data.
+p1 = np.linspace(0, p1_max, 100)
+p2 = np.linspace(0, p2_max, 100)
+X, Y = np.meshgrid(p1, p2)
+
+fig = plt.figure()
 plt.subplots_adjust(bottom=0.25)
-delta = 0.5
+ax = fig.gca(projection='3d')
 
-t = np.arange(0.0, 100.0, 0.1)
-x = np.arange(-3.0, 4.001, delta)
-y = np.arange(-4.0, 3.001, delta)
+def throughput(beta):
+    R1 = np.log2(1 + (g1 * X) / ((g1 * Y * beta + sigma2) * Gamma))
+    R2 = np.log2(1 + (g2 * Y) / ((g2 * X * beta + sigma2) * Gamma))
+    R3d = R1 + R2
+    return R3d
 
-X, Y = np.meshgrid(x, y)
+Z = throughput(0.0)
+surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+ax.zaxis.set_major_locator(LinearLocator(10))
+ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+fig.colorbar(surf, shrink=0.5, aspect=5)
 
-Z1 = bivariate_normal(X, Y, 1.0, 1.0, 0.0, 0.0)
-Z2 = bivariate_normal(X, Y, 1.5, 0.5, 1, 1)
-Z = (Z1 - Z2) * 5.
-
-cmap = plt.cm.rainbow
-im = ax.pcolormesh(X, Y, Z, cmap=cmap)
-fig.colorbar(im)
 axcolor = 'lightgoldenrodyellow'
-axtime = plt.axes([0.25, 0.1, 0.65, 0.03], axisbg=axcolor)
-stime = Slider(axtime, 'Time', 0.0, 100.0, valinit=50.0)
-slider_beta = Slider(axtime, 'Beta', 0.0, 1.0, valinit=0.0)
-
-#Routines to reset and update sliding bar
-def reset(event):
-    stime.reset()
-
-def update(val):
-    time = stime.val/10.
-    Z = (Z1 - Z2) * time
-    im.set_array(Z.ravel())
-    fig.canvas.draw()
+ax_slider = plt.axes([0.25, 0.1, 0.65, 0.03], axisbg=axcolor)
+slider_beta = Slider(ax_slider, 'Beta', 0.0, 1.0, valinit=0.0)
 
 def update_R(val):
-    beta = val
-    
+    Z = throughput(val)
+    ax.clear()
+    surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
 
-
-#Bind sliding bar and reset button
-stime.on_changed(update)
 slider_beta.on_changed(update_R)
-resetax = plt.axes([0.8, 0.025, 0.1, 0.04])
-button = Button(resetax, 'Reset', color=axcolor, hovercolor='0.975')
-button.on_clicked(reset)
 
 plt.show()
 
-def throughput(beta):
+#Bind sliding bar and reset button
+# stime.on_changed(update)
 
-    # Channel conditions
-    g1 = 2.5
-    g2 = 1.5
-    p1_max = 10
-    p2_max = 100
-    sigma2 = 1
-    Gamma = 1
+# resetax = plt.axes([0.8, 0.025, 0.1, 0.04])
+# button = Button(resetax, 'Reset', color=axcolor, hovercolor='0.975')
+# button.on_clicked(reset)
 
-    # Make data.
-    p1 = np.linspace(0, p1_max, 100)
-    p2 = np.linspace(0, p2_max, 100)
-    p1, p2 = np.meshgrid(p1, p2)
+# plt.show()
 
-    R1 = np.log2(1 + (g1 * p1) / ((g1 * p2 * beta + sigma2) * Gamma))
-    R2 = np.log2(1 + (g2 * p2) / ((g2 * p1 * beta + sigma2) * Gamma))
-    R3d = R1 + R2
-    return R3d
